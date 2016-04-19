dd30b6340fb4cfbd2f5516064ba899faf1992b8b
Guest User <coder@mainlab9.local>
2016-04-19 11:32:50 -0700

    dsigddsdsg

---
+++ MadLibGameThing/MadLibsDictionary.swift
@@ -0,0 +1,19 @@
+//
+//  MadLibsDictionary.swift
+//  MadLibGameThing
+//
+//  Created by Coder on 4/19/16.
+//  Copyright © 2016 Alex. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import GameKit
+
+
+struct MadLibDictionary {
+    
+    
+    let madlib1 = "g"
+    
+}
